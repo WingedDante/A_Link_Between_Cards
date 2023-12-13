685a10d0d69c9aa19fa69e270816799d73d18775
LoganT <logan.trecartin@gmail.com>
2023-12-12 23:49:55 -0500

    today updates, adding shuffle, fixing compare, adding real card faces, starting initial player show

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -28,7 +28,7 @@ func _process(delta):
 
 func process_cards_flipped():
 	if (Global.card_to_compare_1 and Global.card_to_compare_2 ):
-		if(Global.card_to_compare_1.texture.resource_name == Global.card_to_compare_2.texture.resource_name):
+		if(Global.card_to_compare_1.cardName == Global.card_to_compare_2.cardName):
 			print("2 CARDS FLIPPED match")
 			score += 1
 			matches += 1
@@ -58,14 +58,57 @@ func game_setup():
 	card_list = []
 	
 func start_game():
+	var cardMatch = 1
+	var random = 0
 	for w in width :
 		for h in height : 
+			var sprite = ""
+			
+			if cardMatch == 1 :
+				var rng = RandomNumberGenerator.new()
+				random = rng.randi_range(1, 7)
+				cardMatch = 2
+			else :
+				cardMatch = 1
 			var newCard = card_scene.instantiate()
-			newCard.position.x = (1745.0/width) * (w) + (1745.0/width)/2
-			newCard.position.y = (1080.0/height) * (h) + (1080.0/height)/2
+			var face = newCard.get_node("Front")
+			if random == 1 :
+				face.texture = load("res://Scenes/Card_Images/Beedle.png")
+				newCard.cardName = "Beedle"
+			elif random == 2 :
+				face.texture = load("res://Scenes/Card_Images/Agahnim.png")
+				newCard.cardName = "Agahnim"
+			elif random == 3 :
+				face.texture = load("res://Scenes/Card_Images/Agitha.png")
+				newCard.cardName = "Agitha"
+			elif random == 4 :
+				face.texture = load("res://Scenes/Card_Images/Alfonzo.png")
+				newCard.cardName = "Alfonzo"
+			elif random == 5 :
+				face.texture = load("res://Scenes/Card_Images/Aryll.png")
+				newCard.cardName = "Aryll"
+			elif random == 6 :
+				face.texture = load("res://Scenes/Card_Images/BeastGanon.png")
+				newCard.cardName = "BeastGanon"
+			elif random == 7 :
+				face.texture = load("res://Scenes/Card_Images/Cucco.png")
+				newCard.cardName = "Cucco"
+			#newCard.position.x = (1745.0/width) * (w) + (1745.0/width)/2
+			#newCard.position.y = (1080.0/height) * (h) + (1080.0/height)/2
 			newCard.game_scene = self
 			card_list.append(newCard)
 			add_child(newCard)
+	card_list.shuffle()
+	var i = 0
+	for w in width :
+		for h in height : 
+			card_list[i].position.x = (1745.0/width) * (w) + (1745.0/width)/2
+			card_list[i].position.y = (1080.0/height) * (h) + (1080.0/height)/2
+			i += 1
+	for card in card_list :
+		card.get_node("AnimationPlayer").play("flip_to_front")
+	
+	
 	game_timer.start()
 
 func _on_game_timer_timeout():
--- global.gd
+++ global.gd
@@ -4,3 +4,5 @@ var card_to_compare_1 : Card
 var card_to_compare_2 : Card
 var card_flipping : bool = false
 var game_over : bool = false
+
+var card_image_list = ["Agahnim.png","Agitha.png","Cucco.png","Alfonzo.png", "Aryll.png", "BeastGanon.png", "Beedle.png"]
